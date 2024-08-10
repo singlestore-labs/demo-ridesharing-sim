863cc29653eb8414702f9847791be966d5e399d8
Bharat Kathi <bharat1031@gmail.com>
2024-08-10 13:15:00 -0700

    cleaned up main

--- simulator/exporter/csv.go
+++ simulator/exporter/csv.go
@@ -189,22 +189,3 @@ func ExportTripsToCSV(trips []models.Trip) {
 		})
 	}
 }
-
-/*
-	ID       string `json:"id"`
-	DriverID string `json:"driver_id"`
-	RiderID  string `json:"rider_id"`
-	// Status can be "requested", "accepted", "en_route", "completed"
-	Status      string    `json:"status"`
-	RequestTime time.Time `json:"request_time"`
-	AcceptTime  time.Time `json:"accept_time"`
-	PickupTime  time.Time `json:"pickup_time"`
-	DropoffTime time.Time `json:"dropoff_time"`
-	Fare        int       `json:"fare"`
-	Distance    float64   `json:"distance"`
-	PickupLat   float64   `json:"pickup_lat"`
-	PickupLong  float64   `json:"pickup_long"`
-	DropoffLat  float64   `json:"dropoff_lat"`
-	DropoffLong float64   `json:"dropoff_long"`
-	City        string    `json:"city"`
-*/
--- simulator/main.go
+++ simulator/main.go
@@ -14,42 +14,14 @@ func main() {
 	database.InitializeLocal()
 	exporter.InitializeKafkaClient()
 
-	// riders, err := exporter.ImportRidersFromCSV("data/riders.csv")
-	// if err != nil {
-	// 	riders = service.GenerateRiders(config.NumRiders, "San Francisco")
-	// }
-	// if len(riders) != config.NumRiders {
-	// 	riders = service.GenerateRiders(config.NumRiders, "San Francisco")
-	// }
-
-	// drivers, err := exporter.ImportDriversFromCSV("data/drivers.csv")
-	// if err != nil {
-	// 	drivers = service.GenerateDrivers(config.NumDrivers, "San Francisco")
-	// }
-	// if len(drivers) != config.NumDrivers {
-	// 	drivers = service.GenerateDrivers(config.NumDrivers, "San Francisco")
-	// }
-
-	// exporter.ExportRidersToCSV(riders)
-	// exporter.ExportDriversToCSV(drivers)
-
 	riders := service.GenerateRiders(config.NumRiders, "San Francisco")
 	drivers := service.GenerateDrivers(config.NumDrivers, "San Francisco")
-
-	// exporter.KafkaProduceTrip(models.Trip{
-	// 	ID:       "ajldas-321jkdsa-sdajkml",
-	// 	RiderID:  "uuidsa-1jpdas0-321",
-	// 	DriverID: "dasdsa-3412d-12sads",
-	// 	Status:   "completed",
-	// 	Distance: 10.2,
-	// 	Fare:     12,
-	// })
 
 	go func() {
 		for _, rider := range riders {
 			database.Local.Riders.Set(rider.ID, rider)
 			go service.StartRiderLoop(rider.ID, "San Francisco")
-			time.Sleep(time.Duration(config.Faker.IntBetween(1, 100)) * time.Millisecond)
+			time.Sleep(time.Duration(config.Faker.IntBetween(1, 1000)) * time.Millisecond)
 		}
 	}()
 
@@ -57,15 +29,10 @@ func main() {
 		for _, driver := range drivers {
 			database.Local.Drivers.Set(driver.ID, driver)
 			go service.StartDriverLoop(driver.ID, "San Francisco")
-			time.Sleep(time.Duration(config.Faker.IntBetween(1, 100)) * time.Millisecond)
+			time.Sleep(time.Duration(config.Faker.IntBetween(1, 1000)) * time.Millisecond)
 		}
 	}()
 
-	// Dump completed trips to CSV every minute
-	// Also acts as a way to keep the main thread alive
-	for {
-		time.Sleep(1 * time.Minute)
-		trips := service.GetTripsByStatus("completed")
-		exporter.ExportTripsToCSV(trips)
-	}
+	// Keep the main thread alive
+	select {}
 }
