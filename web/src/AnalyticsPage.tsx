cb524d4c59ebb16dec166d151185f436d5aaccad
Bharat Kathi <bkathi@singlestore.com>
2024-09-04 03:27:43 -0700

    add toolbar to analytics page

--- web/src/AnalyticsPage.tsx
+++ web/src/AnalyticsPage.tsx
@@ -7,10 +7,11 @@ import TripsMinuteChart from "@/components/analytics/trips-minute-chart";
 import WaitTimeDailyChart from "@/components/analytics/wait-time-daily-chart";
 import WaitTimeHourlyChart from "@/components/analytics/wait-time-hourly-chart";
 import WaitTimeMinuteChart from "@/components/analytics/wait-time-minute-chart";
+import { Toolbar } from "@/components/toolbar";
 
 const AnalyticsPage = () => {
   return (
-    <div className="h-screen w-screen">
+    <div className="relative min-h-screen w-screen overflow-x-hidden">
       <div className="flex w-full flex-col items-start gap-4 p-4">
         <Header currentPage="analytics" />
       </div>
@@ -21,13 +22,16 @@ const AnalyticsPage = () => {
       <div className="flex flex-col items-start p-4">
         <h4>Trends</h4>
       </div>
-      <div className="flex flex-wrap items-center gap-4 px-4">
+      <div className="flex flex-wrap items-center gap-4 px-4 pb-20">
         <TripsMinuteChart />
         <TripsHourlyChart />
         <TripsDailyChart />
         <WaitTimeMinuteChart />
         <WaitTimeHourlyChart />
         <WaitTimeDailyChart />
+      </div>
+      <div className="fixed bottom-4 right-4 z-50">
+        <Toolbar />
       </div>
     </div>
   );
